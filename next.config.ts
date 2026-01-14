f655c1152a2a6bfe61f8ead2e60714400225f5ac
deekshith8900 <deekshithalampally@gmail.com>
2026-01-14 14:53:42 -0800

    config: allow external image domains for seed data

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '**.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'loremflickr.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      }
     ],
   },
 };
